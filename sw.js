7eca788a2b15a7073c4c8fc996a6c55cdfc26a9e
asrayacreate <asrayacreate.ac@gmail.com>
2026-07-26 09:00:31 +0545

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var C="ganak-v4-5";
+var C="ganak-v4-7";
 self.addEventListener("install",function(e){
   e.waitUntil(caches.open(C).then(function(c){
     return c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"]);
